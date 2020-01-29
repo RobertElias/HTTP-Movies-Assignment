9ed5537bbdb64e3adef816c351ca651c41c67113
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-29 15:49:46 -0800

    Add a button in the movie component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -42,6 +42,9 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <button onClick={handleUpdate} className="md-button">
+        Edit
+      </button>
       </div>
     );
   }
